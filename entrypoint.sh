3b3fb4ee0f3e765c89da5e058fded871d15c6bd4
Mithilesh Pawar <Mithilesh.Pawar@checkmarx.com>
2021-05-13 13:56:19 -0400

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
-app/bin/cx scan create --scan-types ${SCAN_TYPES} --project-name ${PROJECT_NAME} -d .  ${ADDITIONAL_PARAMS}
+app/bin/cx scan create --help 
+app/bin/cx scan create --scan-types ${SCAN_TYPES} --project-name ${PROJECT_NAME} ${ADDITIONAL_PARAMS} 
 
