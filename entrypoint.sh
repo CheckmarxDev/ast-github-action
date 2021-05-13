3da0fe2ade6290ef9c4f419c656762f5c005c97e
Mithilesh Pawar <Mithilesh.Pawar@checkmarx.com>
2021-05-13 13:37:08 -0400

    Removed few options

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,2 @@
-#!/bin/sh
+app/bin/cx scan create --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" ${ADDITIONAL_PARAMS}
 
-cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS})"
-
-echo "$cxscan"
-
-cxscan="${cxscan//'%'/'%25'}"
-cxscan="${cxscan//$'\n'/'%0A'}"
-cxscan="${cxscan//$'\r'/'%0D'}"
-echo "::set-output name=cxcli::$cxscan"
